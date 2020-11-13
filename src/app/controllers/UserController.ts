af088e2e391397f47c852778d2b3c509e81bd98c
Christian <alvescastro00cca13@gmail.com>
2020-11-13 11:50:30 -0300

    Criando novo usuário

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -1,8 +1,22 @@
 import { Request, Response } from 'express'
+import { getRepository } from 'typeorm'
 
+import User from '../models/User'
 class UserController {
-    store(req: Request, res: Response) {
-        return res.send('ok')
+    async store(req: Request, res: Response) {
+        const repository = getRepository(User)
+        const { email, password } = req.body
+
+        const userExists = await repository.findOne({ where: {email}})
+
+        if(userExists){
+            return res.sendStatus(409)
+        }
+
+        const user = repository.create({ email, password })
+        await repository.save(user)
+
+        return res.json(user)
     }
 }
 
