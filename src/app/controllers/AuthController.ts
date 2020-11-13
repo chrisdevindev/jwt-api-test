cc1f2c320380649f909a30c52d102908c0f854b6
Christian <alvescastro00cca13@gmail.com>
2020-11-13 12:27:44 -0300

    Utilizando o jwt para criação de token

---
+++ src/app/controllers/AuthController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express'
+import { getRepository } from 'typeorm'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+import User from '../models/User'
+class AuthController {
+    async authenticate(req: Request, res: Response) {
+        const repository = getRepository(User)
+        const { email, password } = req.body
+
+        const user = await repository.findOne({ where: {email}})
+
+        if(!user){
+            return res.sendStatus(401)
+        }
+
+        const isValidPassword = await bcrypt.compare(password, user.password)
+        if(!isValidPassword){
+            return res.sendStatus(401)
+        }
+
+        const token = jwt.sign({ id: user.id }, 'secret', { expiresIn: '10h' })
+
+        delete user.password
+
+        return res.json({
+            user,
+            token
+        })
+
+    }
+}
+
+export default new AuthController()
